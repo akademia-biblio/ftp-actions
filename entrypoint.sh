35baa72b6388782abb8b67c732079909482593c3
akademia-biblio <chrudzimska@snappzone.com>
2021-06-11 22:20:01 +0200

    main

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -l
 
-lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; rm -r public_html/abpl3; mv public_html/abpl1 public_html/abpl2; quit"
+lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false; 
+mv public_html/abpl_current public_html/abpl_old; 
+mv public_html/abpl_load public_html/abpl_current; 
+rm -r public_html/abpl_old;
+quit"
