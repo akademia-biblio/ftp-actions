02618f0601f10efba5635cc8327f040b83c07d3b
akademia-biblio <chrudzimska@snappzone.com>
2021-06-17 17:32:25 +0200

    main

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,11 @@
 
 lftp $INPUT_HOST -u $INPUT_USER,$INPUT_PASSWORD -e "set ftp:ssl-force $INPUT_FORCESSL; set ssl:verify-certificate false;
 mirror $INPUT_OPTIONS --reverse --continue --only-newer --dereference -x ^\.git/$ $INPUT_LOCALDIR $INPUT_REMOTEDIR; 
-mv public_html/abpl public_html/abpl_old; 
-mv public_html/abpl_load public_html/abpl; 
-mv public_html/abpl_old public_html/abpl_load; 
+mv public_html/abpl/_app public_html/abpl/_app_old; 
+mv public_html/abpl_load public_html/abpl/_app;
+mv public_html/abpl/_app_old public_html/abpl/abpl_load; 
 quit"
 
 # rm -r public_html/abpl_old;
 # mkdir public_html/abpl_load;
+# mv public_html/abpl public_html/abpl_old; 
